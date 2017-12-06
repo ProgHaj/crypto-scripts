3a5eae8794e1eca9d5d8e8e0103b22974ffc0fd1
Haj <flummeltrollet@gmail.com>
2017-12-06 21:10:26 +0000

    add argparse to repeating_xor

--- repeating_xor.py
+++ repeating_xor.py
@@ -1,4 +1,38 @@
 import sys
 import hex_operations as ho
+import argparse
 
-print(ho.xor_string(sys.argv[1], sys.argv[2]))
+def repeat_xor_on_file(file_path, xor_string):
+    with open(file_path, 'r') as file_name:
+        read_file = read(file_name)
+        ho.xor_string(file_path, xor_string)
+
+
+def setup_arg_parse():
+    parser = argparse.ArgumentParser(description='This program will do a'
+                                     ' repeated xor on a string/file using'
+                                     ' the provided string.')
+
+    parser.add_argument('--to_be_xored',
+                        required=True,
+                        type=str,
+                        action='store')
+
+    parser.add_argument('--xor_val',
+                        required=True,
+                        type=str,
+                        action='store')
+
+    parser.add_argument('-f',
+                        required=False,
+                        action='store_true')
+
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = setup_arg_parse()
+    if args.f:
+        get_best_sequence_xor_char(args.get(to_be_xored), args.get(xor_val))
+        repeat_xor_on_file(args.to_be_xored, args.xor_val)
+    else:
+        print(ho.xor_string(args.to_be_xored, args.xor_val))
